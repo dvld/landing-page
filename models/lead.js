94fa24e2030a31d3204e8034c2ba9e40bfdd788c
dvld <nikoroberts620@gmail.com>
2019-04-21 21:24:39 -0500

    converting to MERN app; created lead.js model. created index.js in models

---
+++ models/lead.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const leadSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      require: true
+    },
+    subscribed: {
+      type: Boolean
+    }
+  }
+);
+
+const Lead = mongoose.model('Lead', leadSchema);
+
+module.exports = Lead;
