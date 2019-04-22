f1496225c70aabb1c671c169ddf4418da49a0772
dvld <nikoroberts620@gmail.com>
2019-04-21 21:54:00 -0500

    converting to MERN app; created leadsController.js

---
+++ controllers/leadsController.js
@@ -0,0 +1,45 @@
+
+// dependencies
+const db = require('../models');
+
+// methods for leadsController
+module.exports = {
+
+  findAll: function (req, res) {
+    db.Lead
+      .find(req.query)
+      .sort({ name: 1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  findById: function (req, res) {
+    db.Lead
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  create: function (req, res) {
+    db.Lead
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  update: function (req, res) {
+    db.Lead
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+
+  remove: function (req, res) {
+    db.Lead
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+
+};
