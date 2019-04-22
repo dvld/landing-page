0fd39f1037a9afbe0ce5b102c2c9ee10c582c3e3
dvld <nikoroberts620@gmail.com>
2019-04-21 21:06:58 -0500

    converting to MERN app; rewrote server file

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 
 // dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+const routes =require('./routes)');
 
 // application setup
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 // data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-// routes
-require('./public/routes/api.js')(app);
+// add api and view routes
+app.use(routes);
+mongoose.connect(process.env.MONGO_URI || 'mongod://localhost/nikommerce_leads_db');
 
-// listener
+// start api server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
