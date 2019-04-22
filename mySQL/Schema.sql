d2f7621d0ae43ec0e44b62910d2d7ad5c1be6be1
dvld <nikoroberts620@gmail.com>
2019-04-21 21:15:01 -0500

    converting to MERN app; removed mySQL folder

--- mySQL/Schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE superAffiliateSystemLeads;
-USE superAffiliateSystemLeads;
-
-CREATE TABLE leads (
-  leadId INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  PRIMARY KEY (leadId)
-);
